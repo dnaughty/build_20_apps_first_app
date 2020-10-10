415721d634e330797e569d547f2515317046928e
Denis Naughton <mjolinor75@hotmail.com>
2020-10-10 15:44:40 -0400

    Created first app from udemy course build 20 apps

---
+++ build 20 apps first app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  build 20 apps first app
+//
+//  Created by Admin on 10/10/20.
+//  Copyright © 2020 DASHtech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
